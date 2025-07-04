76750b833211a5142c6c8fe9d0518273b42a0d8c
se7en-6xt9 <madhusudanrajput@gmail.com>
2025-07-04 11:13:29 +0530

    add

---
+++ min.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int minoftwo(int a , int b) {
+	if(a<b){ 
+		return a; 
+	} else { 
+		return b;
+		}
+	}
+	
+		int main() {
+	cout<<minoftwo(24,75)<<endl;
+		return 0;
+		}
