c71f45f301beec80b230f12dbadc24295489ab4d
se7en-6xt9 <madhusudanrajput@gmail.com>
2025-07-06 11:40:08 +0530

    This file  contain code to convert binary to decimal

---
+++ binToDecimal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+	int binToDecimal(int binNum){
+	int ans = 0;
+	int  pow = 1;
+	 while(binNum > 0) {
+	 int rem = binNum % 10;
+	 ans = ans + rem * pow ;
+	binNum = binNum / 10 ;
+	 pow = pow * 2 ;
+		}
+	return ans;
+		}
+	int main() {
+		cout<<binToDecimal(1010)<<endl;
+		return 0;
+	}
